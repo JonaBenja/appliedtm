be8c2593b46b01e80ca53d19472b0530c297ac32
JonaBenja <jona.bosman@gmail.com>
2021-01-12 16:59:05 +0100

    finished (dummy) feature_extraction and added (dummy) feature files

--- code/feature_extraction.py
+++ code/feature_extraction.py
@@ -1,25 +1,58 @@
-import nltk
-import spacy
-import pandas
-# from utils import *
 import argparse
 
-
 """
-'../data/SEM-2012-SharedTask-CD-SCO-training-simple.txt'
+'../data/SEM-2012-SharedTask-CD-SCO-training-preprocessed.tsv'
+'../data/SEM-2012-SharedTask-CD-SCO-dev-preprocessed.tsv'
 """
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--file', type=str, required=True)
-args = parser.parse_args()
+def write_features(input_file):
+    """
+    Function that adds features to the preprocessed data and writes it to an output file.
+    :param input_file: path to the preprocessed data file
+    :type input_file: string
+    """
+    # Prepare output file
+    output_file = input_file.replace('.tsv', '-features.tsv')
+    outfile = open(output_file, 'w', encoding='utf-8')
+
+    # Read in preprocessed file
+    with open(input_file, 'r', encoding='utf-8') as infile:
+
+        # Extract token and label from preprocessed file
+        for line in infile:
+            components = line.rstrip('\n').split()
+            token = components[0]
+            label = components[-1]
+
+            # Write token to
+            outfile.write(token + '\t')
+
+            # Write lemma feature value
+            lemma = 'lemma'
+            outfile.write(lemma + '\t')
+
+            # Write POS feature value
+            pos = 'POS'
+            outfile.write(pos + '\t')
+
+            # Write NE feature value
+            ne = 'NE'
+            outfile.write(ne + '\t')
+
+            # Append label to line
+            outfile.write(label + '\n')
+
+    outfile.close()
 
 def main():
-    file = args.file
-    with open(file, 'r') as infile:
-        for line in infile:
-            print(line)
-            break
+    # Set up command line parser
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input_file', type=str, required=True)
+    args = parser.parse_args()
+    input_file = args.input_file
 
+    # Write features to the feature file
+    write_features(input_file)
 
-
-main()
+if __name__ == '__main__':
+    main()
