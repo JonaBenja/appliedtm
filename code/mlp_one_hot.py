9912ca8cb9e79ef15fedd8ca172522167901c25a
JonaBenja <jona.bosman@gmail.com>
2021-01-18 17:15:02 +0100

    added in features

--- code/mlp_one_hot.py
+++ code/mlp_one_hot.py
@@ -1,28 +1,36 @@
 from sklearn.neural_network import MLPClassifier
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.metrics import classification_report
-from sklearn.metrics import accuracy_score
+from collections import defaultdict
 import pandas as pd
-
-
 
 """
 TRAINING
 """
 
-baskerville = '../data/SEM-2012-SharedTask-CD-SCO-training-preprocessed-features.tsv'
+feature_names = ["token",
+                "lemma",
+                "pos_tag",
+                "prev_token",
+                "next_token",
+                "punctuation"]
+
+baskerville = '../data/SEM-2012-SharedTask-CD-SCO-training-preprocessed-features.conll'
 training = pd.read_csv(baskerville, encoding='utf-8', sep='\t')
 
-training_data = [{'token': token} for token in training['token']]
+training_data = []
+
+for i in range(len(training)):
+    feature_dict = defaultdict(str)
+    for feature in feature_names:
+        value = training[feature][i]
+        feature_dict[feature] = value
+
+    training_data.append(feature_dict)
+
+print(len(training_data), training_data[:5])
+
 training_labels = training['gold_label']
-
-print('Sample of training data:')
-print(training_data[:10])
-
-print('Sample of training labels:')
-print(training_labels[:10])
-
-print(len(training_data), len(training_labels))
 
 vec = DictVectorizer()
 x_train = vec.fit_transform(training_data)
@@ -40,19 +48,33 @@ print("Done training network")
 #TESTING
 """
 
-wistoria = '../data/SEM-2012-SharedTask-CD-SCO-dev-preprocessed-features.tsv'
+wistoria = '../data/SEM-2012-SharedTask-CD-SCO-dev-preprocessed-features.conll'
 test = pd.read_csv(wistoria, encoding='utf-8', sep='\t')
 
-test_data = [{'token': token} for token in test['token']]
+
+test_data = []
+
+for i in range(len(test)):
+    feature_dict = defaultdict(str)
+    for feature in feature_names:
+        value = test[feature][i]
+        feature_dict[feature] = value
+
+    test_data.append(feature_dict)
+
+print(len(test_data), test_data[:5])
+
 test_labels = test['gold_label']
 
 x_test = vec.transform(test_data)
 
 prediction = clf.predict(x_test)
 
-metrics = classification_report(test_labels, prediction)
+metrics = classification_report(test_labels, prediction, digits=3)
 print(metrics)
 
+
+"""
 errors = []
 for y_pred, y_true in zip(prediction, test_labels):
     if y_pred != y_true:
@@ -66,3 +88,4 @@ print(errors)
 
 # Word embeddings:
 # 0.9954297508477075
+"""
