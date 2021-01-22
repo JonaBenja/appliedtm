d2335c669da92c9c5a47c2d260b0f6d997105152
JonaBenja <jona.bosman@gmail.com>
2021-01-22 12:33:48 +0100

    update printed info

--- code/data_statistics.py
+++ code/data_statistics.py
@@ -6,7 +6,7 @@ from collections import Counter
 TRAINING DATA
 """
 
-baskerville = '../data/SEM-2012-SharedTask-CD-SCO-training-preprocessed.tsv'
+baskerville = '../data/SEM-2012-SharedTask-CD-SCO-training-preprocessed.conll'
 
 training_data = pd.read_csv(baskerville, encoding = 'utf-8', sep='\t')
 
@@ -24,7 +24,7 @@ for label, token in zip(labels, tokens):
 negation_cues = Counter(n_cues)
 
 print('Training data:')
-print('tokens', len(n_tokens), 'of which', len(tokens)-len(n_tokens), 'are punctuation')
+print('tokens', len(tokens), 'of which', len(tokens)-len(n_tokens), 'are punctuation')
 print('Negation cues')
 print(labeldict)
 print(negation_cues.most_common(10))
@@ -33,7 +33,7 @@ print(negation_cues.most_common(10))
 DEVELOPMENT DATA
 """
 
-wistoria = '../data/SEM-2012-SharedTask-CD-SCO-dev-preprocessed.tsv'
+wistoria = '../data/SEM-2012-SharedTask-CD-SCO-dev-preprocessed.conll'
 dev_data = pd.read_csv(wistoria, encoding = 'utf-8', sep='\t')
 
 tokens = dev_data.iloc[:, 0]
@@ -51,7 +51,7 @@ negation_cues = Counter(n_cues)
 
 print()
 print('Development data:')
-print('tokens', len(n_tokens), 'of which', len(tokens)-len(n_tokens), 'are punctuation')
+print('tokens', len(tokens), 'of which', len(tokens)-len(n_tokens), 'are punctuation')
 print('Negation cues')
 print(labeldict)
 print(negation_cues.most_common(10))
